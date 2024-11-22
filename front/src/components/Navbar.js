0bcb6f39722540c6dedd4d47eb98ff9c676fa803
Your Name <you@example.com>
2024-11-22 15:49:00 +0800

    back

--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <nav>
-            <div>
-                <Link to ="/">CRUD</Link>
+        <nav className ="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="cibtauber">
+                <Link className="navbar-brand" to ="/">CRUD</Link>
                 <div>
-                    <ul>
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li>
                             <Link to ="/add">Add User</Link>
                         </li>
--- front/src/pages/AddUser.js
+++ front/src/pages/AddUser.js
@@ -10,16 +10,40 @@ const AddUser = () => {
 
     });
 
+
+
+    const handleChange= (name) => (e) => {
+        const value = name ==="image"?e.target.files[0]:e.target.value;
+        setData({...data, [name]: value});
+    };
+
+    const handleSubmit= async () => {
+        try{
+            let formData = new FormData();
+            formData.append("image", data.image);
+            formData.append("name", data.name);
+
+            const res = await fetch("http://localhost:3000/user", {method:"PST", body:formData});
+            if(res, ok){
+                setData({name: "", image: "",});
+                navigate("/", {replace: true});
+            }
+        }
+        catch(error){
+            console.log(error);
+        }
+    };
+
     return (
-        <div>
-            <div>
-                <input placeholder="Enter Your Name" type="text" name="name" value={data.name} />
+        <div style={{maxWidth:500, margin:"auto"}}>
+            <div className="mb-3">
+                <input placeholder="Enter Your Name" onChange={handleChange("name")} type="text" name="name" value={data.name} />
             </div>
-            <div>
-                <input type="file" name="image" accept="image/*" />
+            <div className="mb-3">
+                <input type="file" onChange={handleChange("image")} name="image" accept="image/*" />
             </div>
-            <div>
-                <button>Submit</button>
+            <div className="mb-3">
+                <button class="btn btn-primary" onChange={handleSubmit}>Submit</button>
             </div>
         </div>
     );
